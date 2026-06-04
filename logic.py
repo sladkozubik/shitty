32d943615fa6558b95e48fa38cc7b39d7e050a24
Kotikov Ivan <sladkozubik@icloud.com>
2026-06-04 22:08:35 +0300

    life quality options

--- logic.py
+++ logic.py
@@ -1,29 +1,29 @@
-
 import requests
 
+
 def current_weather_in_spb(lat: float, lon: float):
-
     url = "https://api.open-meteo.com/v1/forecast"
-
 
     params = {
         "latitude": lat,
         "longitude": lon,
         "current": "temperature_2m",
     }
-    
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
 
-        temp = data["current"]["temperature_2m"]
-        return temp
-    else:
-        return "Сервер не отвечает"
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f'Ошибка АПИ:{e}')
+        return None
+
+    data = response.json()
+    temp = data["current"]["temperature_2m"]
+    return temp
+
 
 def save_json_data(json_data: str, user_id: int):
-
     user_file = f'data{user_id}.json'
     print(f'saving data to {user_file}')
-    with open(user_file, "w", encoding='utf-8') as f:
+    with open(user_file, "a", encoding='utf-8') as f:
         f.write(json_data)
--- main.py
+++ main.py
@@ -5,47 +5,60 @@ from aiogram.types import Message
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, F, types
 from logic import current_weather_in_spb, save_json_data
+import json
+
 
 load_dotenv('.env')
 
-
 dp = Dispatcher()
+
+#хендлер, сратабывающий на /start. Создает две кнопки на месте ввода текста
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    kb = [[types.KeyboardButton(text="Погода",request_location=True)],[types.KeyboardButton(text='Поделиться данными')]]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    kb = [[types.KeyboardButton(text="Погода", request_location=True)],
+          [types.KeyboardButton(text='Поделиться данными')]]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     await message.answer("Нажмите кнопку ниже:", reply_markup=keyboard)
 
-
-
-
-@dp.message(F.text == 'Поделиться локацией')
-async def use_location(message: Message):
-    await message.answer("Готово")
-
+'''хендлер, срабатывающий на получение локации.
+    можно поделиться своей локацией, нажав на кнопку 'Погода',
+     либо выбрать точку вручную с помощью встроенных инструментов телеги'''
 @dp.message(F.location)
 async def get_location(message: Message):
     latitude = round(message.location.latitude, 4)
-    longitude = round(message.location.longitude,4)
+    longitude = round(message.location.longitude, 4)
     current = current_weather_in_spb(latitude, longitude)
-    await message.answer(f"Погода в регионе: {current}°C ")
+    if current == None:
+        await message.answer("Ошибка получения данных, попробуйте еще раз.")
+    else:
+        try:
+            await message.answer(f"Погода в регионе: {current}°C ")
+        except Exception as e:
+            print(f'telegram error: {e}')
     user = message.from_user
-    print(
-        f"{user.id} @{user.username or 'no_username'} использовал геопозицию:\n"
-        f"lat: {message.location.latitude}\n"
-        f"lon: {message.location.longitude}"
-    )
+
+    result =(f"{user.id} @{user.username or 'no_username'} использовал геопозицию:\n"
+             f"lat: {message.location.latitude}\n"
+             f"lon: {message.location.longitude}\n\n")
+    print(result)
+    os.makedirs("logs", exist_ok=True)
+    with open(
+        f"./logs/{message.from_user.id}.log","a", encoding="utf-8" ) as f:
+        f.write(result)
 
 
-@dp.message(F.text =="Поделиться данными")
+@dp.message(F.text == "Поделиться данными")
 async def get_message_dump(message: Message):
     userid = message.from_user.id
     print(userid)
-    temp = message.model_dump_json(indent=4)
-    print("before save")
+    temp =json.dumps({'id':userid,
+                      'text':message.text}, indent=4, ensure_ascii=False
+                     )
+    print(temp)
+
     save_json_data(temp, userid)
-    print("after save")
     print('handler data works')
+
 
 @dp.message()
 async def register_message(message: Message):
